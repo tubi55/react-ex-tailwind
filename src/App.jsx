b49869987f2934443e14b999c1f43e025160a983
tubi55 <tubi55@nate.com>
2024-05-16 15:44:13 +0900

    tailwind 설치 및 세팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 function App() {
-  return <div></div>;
+  return (
+    <main className="w-full h-screen bg-black/10">
+      <h1>Members</h1>
+      <section>
+        <article>
+          <figure></figure>
+          <div>
+            <h2>David</h2>
+            <p>President</p>
+            <button>View Detail</button>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
