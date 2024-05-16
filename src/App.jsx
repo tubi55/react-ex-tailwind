3fac71cd46c408fe0f4402b0123745dd8a79d5ae
tubi55 <tubi55@nate.com>
2024-05-16 17:02:41 +0900

    hover모션 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ function App() {
   return (
     <main className="w-full h-screen bg-black/5 py-[10vh] px-[10vw]">
       {/* page title */}
-      <h1 className="mb-20 text-6xl font-thin">Members</h1>
+      <h1 className="mb-20 text-6xl font-thin transition hover:text-slate-600">
+        Members
+      </h1>
 
       <section>
         {/* card */}
-        <article className="w-[300px] px-10 py-14 text-center bg-white rounded-xl shadow-lg">
+        <article className="group w-[300px] px-10 pt-14 pb-0 text-center bg-white rounded-xl shadow-lg transition-all opacity-80 hover:opacity-100 hover:pb-14">
           {/* thumbnail */}
           <figure className="relative inline-block mb-6 size-40">
             <img
-              className="absolute object-cover translate-y-4 rounded-full size-full blur-xl saturate-150 brightness-150"
+              className="absolute object-cover transition delay-200 translate-y-4 rounded-full size-full blur-xl saturate-150 brightness-150 "
               src="./img/peter.jpg"
               alt="peter"
             />
@@ -25,7 +27,7 @@ function App() {
           <div>
             <h2 className="text-2xl font-bold">David Choi</h2>
             <p className="text-base mb-7">President</p>
-            <button className="px-8 py-2 mb-4 text-sm text-white rounded-full bg-gradient-to-br from-slate-900 to-slate-600 drop-shadow-2xl">
+            <button className="px-8 py-2 mb-0 text-sm text-white transition delay-150 scale-150 rounded-full opacity-0 bg-gradient-to-br from-slate-900 to-slate-600 drop-shadow-2xl group-hover:opacity-100 group-hover:scale-100">
               View Detail
             </button>
           </div>
