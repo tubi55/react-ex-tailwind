859a99629ce59e75e1d6667f60b05633ee37dce1
tubi55 <tubi55@nate.com>
2024-05-16 18:02:30 +0900

    component 자식요소와 utilities 등록

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,16 +1,8 @@
 function Thumbnail() {
   return (
     <figure className="thumbnail">
-      <img
-        className="absolute object-cover translate-y-4 rounded-full size-full blur-xl saturate-150 brightness-150 "
-        src="./img/peter.jpg"
-        alt="peter"
-      />
-      <img
-        className="absolute object-cover rounded-full size-full"
-        src="./img/peter.jpg"
-        alt="peter"
-      />
+      <img src="./img/peter.jpg" alt="peter" />
+      <img src="./img/peter.jpg" alt="peter" />
     </figure>
   );
 }
