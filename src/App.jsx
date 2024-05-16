a9e2976d728a931c37df0034dfa8b0da1e5afb8e
tubi55 <tubi55@nate.com>
2024-05-16 18:09:38 +0900

    base 레이어에 공통 페이지 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <main className="w-full h-screen bg-black/5 py-[10vh] px-[10vw]">
+    <main>
       {/* page title */}
-      <h1 className="mb-20 text-6xl font-thin transition hover:text-slate-600">
-        Members
-      </h1>
+      <h1 className="title_page">Members</h1>
 
       <section>
         <Card />
