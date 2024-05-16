97bc8499d49975ecbb546bff6d1a72359d3c59b1
tubi55 <tubi55@nate.com>
2024-05-16 17:40:03 +0900

    모션 배경 컴포넌트 분리

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,12 @@
 import Btn from "./Btn";
 import Info from "./Info";
+import MotionBg from "./MotionBg";
 import Thumbnail from "./Thumbnail";
 
 function Card() {
   return (
     <article className="group w-[300px] px-10 pt-14 pb-0 text-center bg-white rounded-xl shadow-lg transition-all opacity-80 hover:opacity-100 hover:pb-14  relative">
-      <div className="absolute top-0 left-0 bg-white opacity-50 size-full group-hover:animate-ping rounded-xl"></div>
+      <MotionBg />
       <Thumbnail />
       <Info />
       <Btn />
---
+++ src/components/MotionBg.jsx
@@ -0,0 +1,7 @@
+function MotionBg() {
+  return (
+    <div className="absolute top-0 left-0 opacity-50 bg-inherit size-full group-hover:animate-ping rounded-xl"></div>
+  );
+}
+
+export default MotionBg;
